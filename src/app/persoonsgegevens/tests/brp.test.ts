06664ab72bb7b65b5f0608c0e2675e086a50133d
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-06-14 15:46:53 +0200

    fix: Validate BSN and use new ApiClient package

--- src/app/persoonsgegevens/tests/brp.test.ts
+++ src/app/persoonsgegevens/tests/brp.test.ts
@@ -1,16 +1,8 @@
 import fs from 'fs';
-import { ApiClient } from '../ApiClient';
+import { ApiClient } from '@gemeentenijmegen/apiclient';
 import { BrpApi } from '../BrpApi';
-import { FileApiClient } from '../FileApiClient';
 
-if (process.env.VERBOSETESTS!='True') {
-  global.console.error = jest.fn();
-  global.console.time = jest.fn();
-  global.console.log = jest.fn();
-}
-
-
-async function getStringFromFilePath(filePath: string) {
+async function getStringFromFilePath(filePath: string): Promise<string> {
   return new Promise((res, rej) => {
     fs.readFile(filePath, (err, data) => {
       if (err) {return rej(err);}
@@ -19,15 +11,12 @@ async function getStringFromFilePath(filePath: string) {
   });
 }
 
-// This test doesn't run in CI by default, depends on unavailable secrets
-test('Mock api', async () => {
-  if (!process.env.CERTPATH || !process.env.KEYPATH || !process.env.CAPATH) {
-    return;
+beforeAll(() => {
+  if (process.env.VERBOSETESTS!='True') {
+    global.console.error = jest.fn();
+    global.console.time = jest.fn();
+    global.console.log = jest.fn();
   }
-  const client = new FileApiClient();
-  const api = new BrpApi(client);
-  const result = await api.getBrpData(12345678);
-  expect(result.Persoon.BSN.BSN).toBe('12345678');
 });
 
 // This test doesn't run in CI by default, depends on unavailable secrets
@@ -40,13 +29,13 @@ test('Api', async () => {
   const ca = await getStringFromFilePath(process.env.CAPATH);
   const client = new ApiClient(cert, key, ca);
   const api = new BrpApi(client);
-  const result = await api.getBrpData(999993653);
+  const result = await api.getBrpData('999993653');
   expect(result.Persoon.BSN.BSN).toBe('999993653');
   expect(result.Persoon.Persoonsgegevens.Naam).toBe('S. Moulin');
 });
 
 // This test doesn't run in CI by default, depends on unavailable secrets
-test('Api', async () => {
+test('Api non-existent', async () => {
   if (
     !process.env.CERTPATH
     || !process.env.KEYPATH
@@ -60,6 +49,7 @@ test('Api', async () => {
   const ca = await getStringFromFilePath(process.env.CAPATH);
   const client = new ApiClient(cert, key, ca);
   const api = new BrpApi(client);
-  const result = await api.getBrpData(12345678);
-  expect(result).toHaveProperty('error');
+  const result = await api.getBrpData('000000097');
+  console.debug(result);
+  expect(result.error).toBe('Het ophalen van gegevens is misgegaan.');
 });
